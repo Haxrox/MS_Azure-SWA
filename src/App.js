ac1ed3b790ac5dc0bd2bead739ea645020d0588f
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-01-15 12:10:27 -0800

    added api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      /*
+      console.log("FETCH");
+      const response = await fetch("https://haxtech.azurewebsites.net/api/ping",
+        {
+          method: "GET"
+        }
+      );
+      console.log(response.json());
+      setData("Hello World!");
+      */
+      (async function () {
+        const { text } = await( await fetch(`/api/message`)).json();
+        setData(text);
+      })();
+    })();
+  });
+
+  return <div>{data}</div>;
 }
 
 export default App;
